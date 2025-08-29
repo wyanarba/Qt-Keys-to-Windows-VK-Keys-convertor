ee6ce5096b5129d3dabc6aaea6747effd8c58fc8
wyanarba <142159056+wyanarba@users.noreply.github.com>
2025-08-29 21:05:34 +0300

    надеюсь кому нибудь пригодится

--- keysConv.h
+++ keysConv.h
@@ -7,16 +7,36 @@
 
 struct KeysConvector
 {
+private:
 	std::unordered_map<Qt::Key, UINT> QKeyToSysKey;
 	std::unordered_map<UINT, Qt::Key> SysKeyToQKey;
 
+public:
 	KeysConvector();
 
 	UINT QtoSys(Qt::Key QKey) {
-		return QKeyToSysKey[QKey];
+
+		auto it = QKeyToSysKey.find(QKey);
+
+		if (it != QKeyToSysKey.end())
+			return it->second;
+		return 0;
 	}
 
 	Qt::Key SysToQ(UINT SysKey) {
-		return SysKeyToQKey[SysKey];
+
+		auto it = SysKeyToQKey.find(SysKey);
+
+		if (it != SysKeyToQKey.end())
+			return it->second;
+
+		return Qt::Key_unknown;
 	}
+
+	UINT QModToSysMod(Qt::KeyboardModifiers mods);
+
+	Qt::KeyboardModifiers SysModToQMod(UINT mods);
 };
+
+
+
